2d3c90c03a746031e4faa92a888298ea5f9d7be9
starla huang <starla_huang@epam.com>
2025-01-21 16:46:31 +0800

    login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 import './globals.css'
+import Login from './login/page'
+import { auth0 } from '@/lib/auth0'
 
-export default function Home() {
-  return <main>Home</main>
+export default async function Home() {
+  // Fetch the user session
+  const session = await auth0.getSession()
+
+  // If no session, show sign-up and login buttons
+  if (!session) {
+    return <Login></Login>
+  }
+
+  // If session exists, show a welcome message and logout button
+  return (
+    <main>
+      <h1>Welcome, {session.user.name}!</h1>
+      <p>
+        <a href='/logout'>
+          <button>Log out</button>
+        </a>
+      </p>
+    </main>
+  )
 }
